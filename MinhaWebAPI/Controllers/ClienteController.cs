c7e6f6691cd12b7e663b3d68b240e700cb84c4ad
VitorCorreaSilva <correa.vitor.silva@gmail.com>
2019-07-06 01:04:37 -0300

    cmt01

---
+++ MinhaWebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MinhaWebAPI.Util;
+using System.Data;
+using MinhaWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace MinhaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {        
+        Autenticacao AutenticacaoServico;
+
+        public ClienteController(IHttpContextAccessor context)
+        {            
+            AutenticacaoServico = new Autenticacao(context);
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Route("listagem")]
+        public List<ClienteModel> Listagem()
+        {
+            return new ClienteModel().Listagem();
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        [Route("cliente/{id}")]
+        public ClienteModel RetornarCliente(int id)
+        {
+            return new ClienteModel().RetornarCliente(id);
+        }
+
+        // POST api/values
+        [HttpPost]        
+        [Route("registrarcliente")]
+        public ReturnAllServices RegistrarCliente([FromBody]ClienteModel dados)
+        {
+            ReturnAllServices retorno = new ReturnAllServices();
+
+            try
+            {
+                dados.RegistrarCliente();
+                retorno.Result = true;
+                retorno.ErrorMessage = string.Empty;
+            }
+            catch(Exception ex)
+            {
+                retorno.Result = false;
+                retorno.ErrorMessage = "Erro ao tentar registrar um cliente: " + ex.Message;
+            }
+
+            return retorno;
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        [Route("atualizar/{id}")]
+        public ReturnAllServices Atualizar(int id, [FromBody]ClienteModel dados)
+        {
+            ReturnAllServices retorno = new ReturnAllServices();
+
+            try
+            {
+                dados.Id = id;
+                dados.AtualizarCliente();
+                retorno.Result = true;
+                retorno.ErrorMessage = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                retorno.Result = false;
+                retorno.ErrorMessage = "Erro ao tentar atualizar um cliente: " + ex.Message;
+            }
+
+            return retorno;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        [Route("excluir/{id}")]
+        public ReturnAllServices Excluir(int id)
+        {
+            ReturnAllServices retorno = new ReturnAllServices();
+            try
+            {
+                retorno.Result = true;
+                retorno.ErrorMessage = "Cliente excluído com sucesso!";
+                AutenticacaoServico.Autenticar();
+                new ClienteModel().Excluir(id);
+
+            }
+            catch(Exception ex)
+            {
+                retorno.Result = false;
+                retorno.ErrorMessage = ex.Message;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ MinhaWebAPI/Util/DAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MinhaWebAPI.Util
+{
+    public class DAL
+    {
+        private static string Server = "sql10.freesqldatabase.com";
+        private static string Database = "sql10297638";
+        private static string User = "sql10297638";
+        private static string Password = "rI3irjRsFR";
+        private MySqlConnection Connection;
+
+        private string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=none;charset=utf8;";
+
+        public DAL() {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+
+        //Executa: INSERT, UPDATE, DELETE
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            Command.ExecuteNonQuery();
+        }
+
+        //Retorna Dados do Banco
+        public DataTable RetornarDataTable(string sql)
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            MySqlDataAdapter DataAdaptar = new MySqlDataAdapter(Command);
+            DataTable Dados = new DataTable();
+            DataAdaptar.Fill(Dados);
+            return Dados;
+        }
+    }
+}
